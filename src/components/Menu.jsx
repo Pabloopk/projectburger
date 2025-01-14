08ac335fcdb3197e820b5aeeb88059ed3dfdafcf
Pablo <parkour.pablo458@gmail.com>
2025-01-14 11:34:00 -0300

    add  margin burger

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,31 +7,39 @@ const Menu = () => {
       id: 1,
       title: "Tango Burger",
       price: "$29",
+      text: "2 hamburguer, ovo, salsicha, alface, tomate  queijo",
+      
     },
     {
       id: 2,
-      title: "Punch Burger",
       price: "$25",
+      title: "Punch Burger",
+      text: "2 hamburguer, ovo, salsicha, alface, tomate  queijo",
+      
     },
     {
       id: 3,
       title: "Crunch Burger",
       price: "$20",
+      text: "2 hamburguer, ovo, salsicha, alface, tomate  queijo",
     },
     {
       id: 4,
       title: "Carnival Burger",
       price: "$42",
+      text: "2 hamburguer, ovo, salsicha, alface, tomate  queijo",
     },
     {
       id: 5,
       title: "Twist Burger",
       price: "$40",
+      text: "2 hamburguer, ovo, salsicha, alface, tomate  queijo",
     },
     {
       id: 6,
       title: "Burst Burger",
       price: "$45",
+      text: "2 hamburguer, ovo, salsicha, alface, tomate  queijo",
     },
   ];
 
@@ -44,10 +52,10 @@ const Menu = () => {
         </h1>
 
         {/* cards section */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 pt-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 text-[12px] pt-8">
           {menuData.map((item) => {
             return (
-              <MenuCard key={item.id} title={item.title} price={item.price} />
+              <MenuCard key={item.id} title={item.title} price={item.price} text={item.text} />
             );
           })}
         </div>
